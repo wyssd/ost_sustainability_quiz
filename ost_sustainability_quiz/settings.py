47601511b064598e339147630b55df4cf4c3baf3
Lukas Gstöhl <l.gstoehl@gmail.com>
2025-04-19 16:25:07 +0200

    Lokale Änderung an settings.py

--- ost_sustainability_quiz/settings.py
+++ ost_sustainability_quiz/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = 'django-insecure-_mblplqk$#tzskt*lb3v%hiichxl)e)-t&bw3erxwael0=wcnd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+]
 
 
 # Application definition
